7b424e74418bd707ed2b9bf2a8262c5a21024f7d
KamikazeZirou <kamikaze.zirou@gmail.com>
2020-11-20 04:55:58 +0900

    chore: ビルド設定の調整

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     kotlin("jvm")
     kotlin("kapt")
     id("maven")
+    id("maven-publish")
 }
 
-group="com.github.KamikzeZirou"
+group = "com.github.KamikzeZirou"
 version = "0.0.1-SNAPSHOT"
 
 sourceSets {
@@ -20,4 +21,20 @@ dependencies {
     implementation("com.google.auto.service:auto-service:1.0-rc7")
     implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.2.0")
     kapt("com.google.auto.service:auto-service:1.0-rc7")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.KamikzeZirou"
+            artifactId = "kotlin-fast-mock"
+            version = "0.0.1"
+            from(components["kotlin"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("file://$buildDir/repos")
+        }
+    }
 }
