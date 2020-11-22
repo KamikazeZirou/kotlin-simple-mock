4affab930ad965f6bfbc148c492f008bf1da8403
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2020-11-23 02:22:36 +0900

    doc

--- sample/src/mock/fast/kotlin/sample/Generics.kt
+++ sample/src/mock/fast/kotlin/sample/Generics.kt
@@ -7,4 +7,6 @@ interface Generics<out K, in V> {
     fun get(id: Int): K
     fun getAll(): List<K>
     fun add(elm: V)
+//    Generic functions are not supported.
+//    fun <T> singletonList(item: T): List<T>
 }
