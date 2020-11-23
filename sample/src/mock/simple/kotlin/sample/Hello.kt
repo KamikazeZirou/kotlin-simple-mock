0d7e69978bf1743e4f7d51e6f38043aada3eea97
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2020-11-24 02:20:16 +0900

    tweak

--- sample/src/mock/simple/kotlin/sample/Hello.kt
+++ sample/src/mock/simple/kotlin/sample/Hello.kt
@@ -2,14 +2,8 @@ package mock.simple.kotlin.sample
 
 import mock.simple.kotlin.Mockable
 
-class Hoge()
-
 @Mockable
 interface Hello {
-    val num: Int
-    var sum: Int
-    fun foo(): Hoge
-    fun bar(value: Int): Int
-    fun add(a: Float, b: Float): Float
-    fun gets(a: List<String>): List<String>
+    fun add(a: Int, b: Int): Int
+    var num: Int
 }
